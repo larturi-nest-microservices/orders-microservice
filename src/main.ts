1c1ba0d26608d3b9ae7bbe393ae2f7a1f6968766
Leandro Arturi <lea.arturi@gmail.com>
2024-04-15 19:37:01 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './configs/envs';
+
+async function bootstrap() {
+  const logger = new Logger('OrdersMain');
+
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port);
+  logger.log(`Orders Microservice running on port ${envs.port}`);
+}
+bootstrap();
